828e55aaa006d5a5301b586ed09188d0d82bac29
iRiver-0023 <iriver0023@outlook.com>
2026-06-07 04:19:18 +0800

    更新评论功能

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -2,7 +2,7 @@ import type { CommentConfig } from "../types/config";
 
 export const commentConfig: CommentConfig = {
 	// 评论系统类型: none, twikoo, waline, giscus, disqus, artalk，默认为none，即不启用评论系统
-	type: "waline",
+	type: "twikoo",
 
 	//twikoo评论系统配置
 	twikoo: {
@@ -36,7 +36,7 @@ export const commentConfig: CommentConfig = {
 		//   'enable'   —— 默认，允许访客匿名评论和用第三方 OAuth 登录评论，兼容性最佳。
 		//   'force'    —— 强制必须登录后才能评论，适合严格社区，关闭匿名评论。
 		//   'disable'  —— 禁止所有登录和 OAuth，仅允许匿名评论（填写昵称/邮箱），适用于极简留言。
-		login: "enable",
+		login: "disable",
 		// 是否启用文章访问量统计功能
 		visitorCount: true,
 	},
