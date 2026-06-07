93a6ac68e3fcb67f50e7febf0c080a2bd211e67c
iRiver-0023 <iriver0023@outlook.com>
2026-06-08 02:41:13 +0800

    更新浏览器标签页图标

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -57,7 +57,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "assets/images/firefly.png",
+			src: "/favicon/favicon.ico",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
