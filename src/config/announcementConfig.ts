7039461d49dac1423cfa1d8872f4290f5c784964
iRiver-0023 <iriver0023@outlook.com>
2026-06-10 01:15:42 +0800

    更新

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,10 +2,10 @@ import type { AnnouncementConfig } from "../types/config";
 
 export const announcementConfig: AnnouncementConfig = {
 	// 公告标题
-	title: "高考加油",
+	title: "公告",
 
 	// 公告内容
-	content: "十年磨一剑，今朝试锋芒。高考必胜！笔下生辉！",
+	content: "十年磨一剑，今朝试锋芒。祝：笔下生辉！",
 
 	// 是否允许用户关闭公告
 	closable: false,
@@ -14,9 +14,9 @@ export const announcementConfig: AnnouncementConfig = {
 		// 启用链接
 		enable: true,
 		// 链接文本
-		text: "了解更多",
+		text: "关于我",
 		// 链接 URL
-		url: "/posts/guide/致每一个走进考场的你/",
+		url: "/about/",
 		// 内部链接
 		external: false,
 	},
